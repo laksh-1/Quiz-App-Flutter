704b6552e82786be6d59560de9c9369bf36d11b6
Laksh Mittal <mittallaksh1@gmail.com>
2021-11-04 17:57:43 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quiz_brain.dart';
+
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Take a Quiz'),
+          backgroundColor: Colors.grey,
+        ),
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  Quiz_Brain quizBrain = Quiz_Brain();
+  int questionNo = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.questionBank[questionNo].questionText,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                Colors.green,
+              )),
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  if (quizBrain.questionBank[questionNo].questionAnswer ==
+                      true) {
+                    scoreKeeper.add(const Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ));
+                  } else {
+                    scoreKeeper.add(const Icon(
+                      Icons.clear,
+                      color: Colors.red,
+                    ));
+                  }
+                  if (questionNo < 12) {
+                    questionNo = (questionNo + 1);
+                  } else {
+                    questionNo = 0;
+                    scoreKeeper.clear();
+                    Alert(
+                            context: context,
+                            title: "End of Quiz",
+                            desc: "You have attempted all the questions.")
+                        .show();
+                  }
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                Colors.red,
+              )),
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  if (quizBrain.questionBank[questionNo].questionAnswer ==
+                      false) {
+                    scoreKeeper.add(const Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ));
+                  } else {
+                    scoreKeeper.add(const Icon(
+                      Icons.clear,
+                      color: Colors.red,
+                    ));
+                  }
+                  if (questionNo < 12) {
+                    questionNo = (questionNo + 1);
+                  } else {
+                    questionNo = 0;
+                    scoreKeeper.clear();
+                    Alert(
+                            context: context,
+                            title: "End of Quiz",
+                            desc: "You have attempted all the questions.")
+                        .show();
+                  }
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
